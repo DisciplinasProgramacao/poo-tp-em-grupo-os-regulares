4d9062aa825d727f0f066e371d92283d72b63661
Ana Luíza <113112185+Analuizaleite@users.noreply.github.com>
2023-09-29 09:11:24 -0300

    Add files via upload

---
+++ codigo/Frota.java
@@ -0,0 +1,27 @@
+
+public class Frota {
+
+	private int tamanhoFrota;
+	private Veiculo veiculos[];
+	
+	public Frota() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public String relatorioFrota() {
+		
+	}
+	public Veiculo localizarVeiculo (String placa) {
+		
+	}
+	public double quilometragemTotal () {
+		
+	}
+	public Veiculo maiorKmTotal() {
+		
+	}
+	public Veiculo maiorKmMedia() {
+		
+	}
+
+}
